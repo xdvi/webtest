f744fe1515e016e5db781d5c39521d80d29ab7a4
Daabi <73258589+xdvi@users.noreply.github.com>
2024-08-24 00:08:42 -0500

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins: [
+      // Usar Tailwind CSS
+      require('tailwindcss'),
+      // Usar Autoprefixer para agregar prefijos de navegador automáticamente
+      require('autoprefixer')
+    ]
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js, ts}"],
+  // content: [
+  //   './src/**/*.html',
+  //   './src/**/*.ts',
+  //   './src/**/*.js',
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // require('tailwindcss'),
+    // require('autoprefixer'),
+  ],
+}
+
