20f0a7c828209ffd0b12d13f723826e3676f2c5b
Daabi <73258589+xdvi@users.noreply.github.com>
2024-10-09 20:13:22 -0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,20 @@ import { setupWebSocket } from './ws/index';
 
 // Crea el servidor HTTP
 const server = createHttpServer();
+const production = false;
+
+let ssl = true;
+if (!production && ssl) {
+  ssl = false
+}
+
+const url = 'webtest-j76z.onrender.com';
+const port = 8080;
 
 // Configura WebSocket en el servidor HTTP
 setupWebSocket(server);
 
 // Inicia el servidor en el puerto 8080
-server.listen(8080, () => {
-  console.log('Servidor escuchando en http://localhost:8080');
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor escuchando en http://0.0.0.0:${port}`);
 });
